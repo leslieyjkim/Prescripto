9e4816136a3a2ecf2eb0ffa2c84fdb8fc0f11370
yoonjookim <yjblossom@gmail.com>
2024-09-11 13:51:46 -0400

    Add pages and setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='text-green-500'>
-      Testing Tailwind
+    <div className='mx-4 sm:mx-[10%]'>
+      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+//create with writing 'rafce'
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
