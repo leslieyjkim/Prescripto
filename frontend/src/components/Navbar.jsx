f60d90ff737c90756f1a7a84e9b3a618fd2c3d64
yoonjookim <yjblossom@gmail.com>
2024-09-11 20:44:53 -0400

    Feature NavBar (2) Loggedin:profile_picture with dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [token, setToken] = useState(true)
+    //when we don't have token, we're logged out. 
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
@@ -25,7 +32,22 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex item-center gap-4'>
-        <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+        {
+            token 
+            ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                <img className='w-2.5' src={assets.dropdown_icon} alt=""/>
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                        <p className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p className='hover:text-black cursor-pointer'>My Appointments</p>
+                        <p className='hover:text-black cursor-pointer'>Logout</p>
+                    </div>
+                </div>
+            </div>
+            : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+        }
+        
       </div>
     </div>
   )
