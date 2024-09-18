71c7c1aa566a1abd3f3fee1f7aeac5732ba1ec20
yoonjookim <yjblossom@gmail.com>
2024-09-18 17:36:53 -0400

    Feature Contact page (1)

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div>
+        <img src={assets.contact_image} alt=""/>
+        <div>
+          <p>OUR OFFICE</p>
+          <p>54709 Willms Station <br /> Suite 305, Washington, USA</p>
+          <p>Tel:(123)456-7890 <br /> Email: prescripto_a@gamil.com</p>
+          <p>CAREERS AT PRESCRIPTO</p>
+          <p>Learn more about our teams and job openings.</p>
+          <button>Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Contact
+ 
