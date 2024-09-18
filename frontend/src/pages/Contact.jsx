278aea5122a4931d7d0b97ea35528bee0ed64263
yoonjookim <yjblossom@gmail.com>
2024-09-18 17:41:47 -0400

    Feature Contact page with styling (2)

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,15 +8,15 @@ const Contact = () => {
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
       </div>
 
-      <div>
-        <img src={assets.contact_image} alt=""/>
-        <div>
-          <p>OUR OFFICE</p>
-          <p>54709 Willms Station <br /> Suite 305, Washington, USA</p>
-          <p>Tel:(123)456-7890 <br /> Email: prescripto_a@gamil.com</p>
-          <p>CAREERS AT PRESCRIPTO</p>
-          <p>Learn more about our teams and job openings.</p>
-          <button>Explore Jobs</button>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt=""/>
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>54709 Willms Station <br /> Suite 305, Washington, USA</p>
+          <p className='text-gray-500'>Tel:(123)456-7890 <br /> Email: prescripto_a@gamil.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
     </div>
