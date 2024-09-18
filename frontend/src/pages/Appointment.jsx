3be580911fed32e418f31b8d8e3e2c0d99264be2
yoonjookim <yjblossom@gmail.com>
2024-09-18 10:12:53 -0400

    Feature Appointment page on specific doctor's page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [docInfo, setDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(() => {
+    fetchDocInfo()
+  },[doctors,docId])
+
+  return ( 
     <div>
-      
-    </div>
+     {/* ------- Doctor Details ------- */}
+     <div>
+      <div>
+        <img src={docInfo.image} alt=""/>
+      </div>
+     </div>
+    </div>  
   )
 }
 
