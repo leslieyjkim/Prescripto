77edbbeffe1c17a7ff64ff40709323e4229b8a39
yoonjookim <yjblossom@gmail.com>
2024-09-18 16:36:58 -0400

    Feature Related Doctors on Appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
 
@@ -135,6 +136,9 @@ const Appointment = () => {
 
       <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 '>Book an appointment</button>
      </div>
+
+     {/* ------ Listing Related Doctors ------ */}
+     <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>  
   )
 }
