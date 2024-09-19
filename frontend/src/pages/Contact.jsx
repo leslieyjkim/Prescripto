a5def20a8872612558dbf88fd12b12d682417607
yoonjookim <yjblossom@gmail.com>
2024-09-18 22:52:11 -0400

    Fixed sample email on Contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className='text-gray-500'>54709 Willms Station <br /> Suite 305, Washington, USA</p>
-          <p className='text-gray-500'>Tel:(123)456-7890 <br /> Email: prescripto_a@gamil.com</p>
+          <p className='text-gray-500'>Tel:(123)456-7890 <br /> Email: sample@sample.com</p>
           <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
