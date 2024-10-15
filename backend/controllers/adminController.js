8f166822a76e14138da83bf47b49198dc1fb545a
yoonjookim <yjblossom@gmail.com>
2024-10-15 15:37:22 -0400

    Setup controllers, middlewares, routes with adding onto server.js

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,21 @@
+
+
+
+
+//API for adding doctor
+const addDoctor = async () => {
+
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.imageFile
+
+        console.log({ name, email, password, speciality, degree, experience, about, fees, address }, imageFile);
+
+    } catch (error) {
+
+    }
+
+}
+
+export {addDoctor}
