6ca1ebb22d1b1cc5ce0090276cf84ea92d789733
yoonjookim <yjblossom@gmail.com>
2024-10-29 12:45:15 -0400

    Feature Logout/Login function of Navbar button on Admin page

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -5,7 +5,12 @@ import { AdminContext } from '../context/AdminContext'
 
 const NavBar = () => {
 
-    const { aToken } = useContext(AdminContext)
+    const { aToken,setAToken } = useContext(AdminContext)
+
+    const logout = () => {
+        aToken && setAToken('')
+        aToken && localStorage.removeItem('aToken')
+    }
 
     return (
         <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
@@ -13,7 +18,7 @@ const NavBar = () => {
                 <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
                 <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
             </div>
-        <button className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+        <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
         </div>
 
   )
