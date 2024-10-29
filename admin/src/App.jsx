3654767f984f5da0821b9ad939d89e9bc1620aef
yoonjookim <yjblossom@gmail.com>
2024-10-29 12:42:37 -0400

    Feature NavBar in Admin page (1)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,14 +3,16 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
+import NavBar from './components/NavBar';
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer />
+      <NavBar />
 
     </div>
   ) : (
