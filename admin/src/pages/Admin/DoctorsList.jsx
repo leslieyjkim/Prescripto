c564b356b09b2033627fc3749dbac9641b48ae7a
yoonjookim <yjblossom@gmail.com>
2024-11-30 16:21:50 -0500

    Update DoctorsList in admin page to call all-doctors

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
+
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext)
+
+  useEffect(()=>{
+    if(aToken) {
+      getAllDoctors()
+    }
+  },[aToken])
+
   return (
     <div>
-      
+
     </div>
   )
 }
