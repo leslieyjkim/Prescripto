43dc59d56fba97b1c7b5bc6973f2fb01b6a8f974
yoonjookim <yjblossom@gmail.com>
2024-12-12 15:36:33 -0500

    Style Doctor panel : Doctor Profile page (5)

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -19,46 +19,46 @@ const DoctorProfile = () => {
   return profileData && (
     <div>
       
-      <div>
+      <div className='flex flex-col gap-4 m-5'>
         <div>
-          <img src={profileData.image} alt=""/>
+          <img className='bg-primary/80 w-full sm:max-w-64 rounded-lg' src={profileData.image} alt=""/>
         </div>
 
-        <div>
+        <div className='flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-white'>
           {/* Doc Info: name, degree, experience */}
-          <p>{profileData.name}</p>
-          <div>
+          <p className='flex items-center gap-2 text-3xl font-medium text-gray-700'>{profileData.name}</p>
+          <div className='flex itmes-center gap-2 mt-1 text-gray-600'>
             <p>{profileData.degree} - {profileData.speciality}</p>
-            <button>{profileData.experience}</button>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{profileData.experience}</button>
           </div>
 
           {/* Doc About */}
           <div>
-            <p>About:</p>
-            <p>
+            <p className='flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3'>About:</p>
+            <p className='text-sm text-gray-600 max-w-[700px] mt-1'>
               {profileData.about}
             </p>
           </div>
 
-          <p>
-            Appointment fee: <span>{currency} {profileData.fees}</span>
+          <p className='text-gray-600 font-medium mt-4'>
+            Appointment fee: <span className='text-gray-800'>{currency} {profileData.fees}</span>
           </p>
 
-          <div>
+          <div className='flex gap-2 py-2'>
             <p>Address:</p>
-            <p>
+            <p className='text-sm'>
               {profileData.address.line1}
               <br />
               {profileData.address.line2}
             </p>
           </div>
 
-          <div>
+          <div className='flex gap-1 pt-2'>
              <input type="checkbox" name="" id="" />  
              <label htmlFor="">Available</label>
           </div> 
 
-          <button>Edit</button>
+          <button className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all'>Edit</button>
 
         </div>
       </div>
