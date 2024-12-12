dd3191ad8d3b3a730ec5bec7e2da9dcc8e8c400a
yoonjookim <yjblossom@gmail.com>
2024-12-12 14:56:28 -0500

    Feature Doctor panel : Doctor Profile page (3)

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -12,6 +12,7 @@ const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
 
     // Getting Doctor appointment data from Database using API
     const getAppointments = async () => {
@@ -85,6 +86,23 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+
+    const getProfileData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/profile', {headers:{dToken}})
+            if (data.success) {
+                setProfileData(data.profileData)
+                console.log(data.profileData)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+
     const value = {
         dToken, setDToken,
         backendUrl,
@@ -92,7 +110,8 @@ const DoctorContextProvider = (props) => {
         getAppointments,
         completeAppointment,
         cancelAppointment,
-        dashData, setDashData, getDashData
+        dashData, setDashData, getDashData,
+        profileData, setProfileData, getProfileData,
     }
 
     return (
